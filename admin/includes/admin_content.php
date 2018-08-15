da96b00972ef5e1995870bdf3cf89b4042a40c9f
TADAS-PC\taaas <tadas.mockvicius@gmail.com>
2018-08-15 19:28:16 +0300

    first commit

---
+++ admin/includes/admin_content.php
@@ -0,0 +1,39 @@
+<div class="container-fluid">
+    <!-- Page Heading -->
+    <div class="row">
+        <div class="col-lg-12">
+            <h1 class="page-header">
+                Admin
+                <small>Subheading</small>
+            </h1>
+            <?php
+
+
+//            $user = new User();
+//            $result_set = User::find_all_users();
+//            while ($row = mysqli_fetch_array($result_set)){
+//            echo $row['username'] . "<br>";
+            //
+            //            }
+
+            //            $found_user = User::find_user_by_id(2);
+            //$user = User::instantation($found_user);
+            //
+            //            echo $user->id;
+
+
+            ?>
+
+            <ol class="breadcrumb">
+                <li>
+                    <i class="fa fa-dashboard"></i> <a href="index.html">Dashboard</a>
+                </li>
+                <li class="active">
+                    <i class="fa fa-file"></i> Blank Page
+                </li>
+            </ol>
+        </div>
+    </div>
+    <!-- /.row -->
+</div>
+<!-- /.container-fluid -->
